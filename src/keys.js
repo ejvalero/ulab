1bbd5f1ea03d4c69ca306e57dfe5f4eb5fd65d61
ejvalero <ejvalero10@gmail.com>
2022-09-28 23:40:07 -0400

    Upadate db host

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
   database: {
-    host:     'emanuelvalero.com',
-    user:     'emanuelv_ulabv00',
-    password: '7xIJ9CGxc',
-    database: 'emanuelv_ulabv00'
+    host:     'sql204.epizy.com',
+    user:     'epiz_32688357',
+    password: 'vH2qI5taJ4',
+    database: 'epiz_32688357_ulab0'
   },
 }
